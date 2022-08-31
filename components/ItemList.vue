09deb8403fab1640dce47cda4ae398eb53a8732a
gsm-abdullahi <onikokoabdullahi@gmail.com>
2022-08-31 16:48:02 +0100

    Initial commit

---
+++ components/ItemList.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="item-list">
+  hello
+</div>
+</template>
+
+<script>
+export default {
+name: 'ItemList'
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Tutorial />
+    <ItemList />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
