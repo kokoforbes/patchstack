25cb2aa82cc1beef33d6c56f824c3be3498c4b51
gsm-abdullahi <onikokoabdullahi@gmail.com>
2022-09-02 10:40:34 +0100

    Implemented text overflow ellipsis for itemList component

--- components/ItemList.vue
+++ components/ItemList.vue
@@ -1,16 +1,18 @@
 <template>
-  <div
-    class="item-list my-8 bg-white overflow-hidden shadow sm:rounded-lg p-6"
-    @click="viewVulnerability()"
+  <nuxt-link
+    :to="{ name: 'details', params: { vulnerability: this.vulnerability } }"
   >
-    <h2 class="text-2xl leading-7 font-semibold">
-      Welcome to your Nuxt Application
-    </h2>
-    <p class="mt-3 text-gray-600">
-      We recommend you take a look at the, whether you are new or have previous
-      experience with the framework.<br />
-    </p>
-  </div>
+    <div
+      class="item-list flex flex-col flex-grow my-8 bg-white shadow rounded-lg p-6"
+    >
+      <h2 class="text-2xl leading-7 font-semibold">
+        {{ vulnerability.title }}
+      </h2>
+      <p class="mt-3 text-gray-600">
+        {{ vulnerability.description }}
+      </p>
+    </div>
+  </nuxt-link>
 </template>
 
 <script>
@@ -25,27 +27,22 @@ export default {
       },
     },
   },
-
-  methods: {
-    viewVulnerability() {
-      this.$router.push({
-        path: `/edit`,
-        append: true,
-        params: {
-          vulnerability: this.vulnerability,
-        },
-      })
-    },
-  },
-
-  mounted() {},
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .item-list {
   max-width: 50rem;
   width: 100%;
-  // height: 6rem;
+
+  p {
+    width: 15rem;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    @media (min-width: 500px) {
+      width: 30rem;
+    }
+  }
 }
 </style>
