b82cfbadac1bad8bd0d2ff598aa88e3cb75a290c
gsm-abdullahi <onikokoabdullahi@gmail.com>
2022-09-02 11:08:41 +0100

    Created default layout page and transition animation

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <transition name="fade">
+      <Nuxt id="maincontent" :key="$route.fullPath" />
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Default',
+}
+</script>
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
